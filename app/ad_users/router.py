824655879597af9cdb4536f1eaf6c95daaac0b9d
adminer <1@1.RU>
2023-10-12 16:57:18 +0300

    router for create ad_user +

--- app/ad_users/router.py
+++ app/ad_users/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, status, Response, Depends
 
+from app.ad_users.dao import AdUsersDAO
 from app.api_users.auth import get_password_hash, verify_password, authenticate_api_user, create_access_token
 from app.api_users.dependencies import get_current_api_user
 from app.api_users.models import ApiUsers
@@ -21,6 +22,6 @@ router = APIRouter(
 
 @router.post('/add_new_user')
 async def add_new_ad_user(api_user: ApiUsers = Depends(get_current_api_user)):
-    # print(user, type(user), user.email)
+    # print(user, type(user), user.)
     # return await BookingDAO.find_all(user_id=user.id)
-    return await ApiUsersDAO.find_all(id=1)
+    return await AdUsersDAO.find_all(id=1)
--- app/ad_users/shemas.py
+++ app/ad_users/shemas.py
@@ -1,19 +1,16 @@
+from datetime import date
+
 from pydantic import BaseModel, EmailStr
 
 
-class SAdUser(BaseModel):
-    login: str
-    password: str
 
-    # id: Mapped[int] = mapped_column(primary_key=True)
-    # api_user_id: Mapped[int]
-    # first_name: Mapped[str]
-    # middle_name: Mapped[str]
-    # last_name: Mapped[str]
-    # login_name: Mapped[Optional[str]]
-    # department: Mapped[str]
-    # job_title: Mapped[str]
-    # tabel_number: Mapped[int]
-    # action_data: Mapped[Optional[timestamp]] = mapped_column(default=datetime.utcnow())
-    # # action_data: Mapped[Date] = mapped_column(Date)
-    # action: Mapped[str]
+class SAdUser(BaseModel):
+
+    first_name: str
+    middle_name: str
+    last_name: str
+    login_name: str
+    department: str
+    job_title: str
+    tabel_number: int
+    action_data: date
